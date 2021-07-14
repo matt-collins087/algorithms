f3cbf6bcba64c6f7110c9fdc830e0ba77dd291c9
Matt Collins <70385445+matt-collins087@users.noreply.github.com>
2021-07-14 11:12:54 -0500

    Time: 108 ms (41.26%), Space: 41.7 MB (51.49%) - LeetHub

---
+++ maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    nums.sort((a, b) => {
+        return b - a;
+    })
+    
+    return (nums[0] * nums[1]) - (nums[nums.length - 2] * nums[nums.length - 1]);
+};
