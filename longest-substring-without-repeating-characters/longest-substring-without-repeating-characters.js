80506857ea7ab87f9c0af58389e6551f2a6b0d0e
Matt Collins <70385445+matt-collins087@users.noreply.github.com>
2021-08-03 13:18:42 -0500

    Time: 92 ms (94.88%), Space: 43.3 MB (75.61%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0
+    let right = 0
+    let set = new Set();
+    let result = 0;
+
+    while(right < s.length) {
+        if(!set.has(s[right])) {
+            set.add(s[right++]);
+            result = Math.max(result, right - left);
+        } else {
+            set.delete(s[left++])
+        }
+    }
+    return result
+};
